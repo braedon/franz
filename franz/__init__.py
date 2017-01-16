2f48c1ecbd9c69f5b0ad2d9e66f66dd5d58c09c0
Braedon Vickers <braedon.vickers@gmail.com>
2017-01-16 19:34:20 +1300

    Add produce command

--- franz/__init__.py
+++ franz/__init__.py
@@ -3,7 +3,28 @@ import json
 import logging
 import sys
 
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
+
+
+def key_serializer(key):
+    if isinstance(key, str):
+        return key.encode('utf-8')
+
+
+def value_serializer(value):
+    if isinstance(value, str):
+        return value.encode('utf-8')
+
+
+def key_deserializer(key):
+    if key is not None:
+        return key.decode('utf-8')
+
+
+def value_deserializer(value):
+    if value is not None:
+        return value.decode('utf-8')
+
 
 CONTEXT_SETTINGS = {
     'help_option_names': ['-h', '--help']
@@ -64,8 +85,8 @@ def consume(topic,
     consumer = KafkaConsumer(
         *topic,
         bootstrap_servers=bootstrap_brokers,
-        value_deserializer=lambda v: v.decode('utf-8'),
-        key_deserializer=lambda k: k.decode('utf-8'),
+        value_deserializer=value_deserializer,
+        key_deserializer=key_deserializer,
         auto_offset_reset='earliest' if default_earliest_offset else 'latest',
         consumer_timeout_ms=fetch_timeout,
         group_id=consumer_group
@@ -109,4 +130,72 @@ def consume(topic,
     consumer.close()
 
 
+@click.command()
+@click.argument('topic')
+@click.option('-b', '--bootstrap-brokers', default='localhost',
+              help='Addresses of brokers in a Kafka cluster to talk to.' +
+                   ' Brokers should be separated by commas' +
+                   ' e.g. broker1,broker2.' +
+                   ' Ports can be provided if non-standard (9092)' +
+                   ' e.g. broker1:9999. (default: localhost)')
+@click.option('-j', '--json-value', is_flag=True,
+              help='Parse message values as JSON.')
+@click.option('-v', '--verbose', is_flag=True,
+              help='Turn on verbose logging.')
+def produce(topic,
+            bootstrap_brokers,
+            json_value,
+            verbose):
+    '''Produce messages to a Kafka topic.
+       By default, connect to a kafka cluster at localhost:9092.'''
+
+    logging.basicConfig(
+        format='[%(asctime)s] %(name)s.%(levelname)s %(threadName)s %(message)s',
+        level=logging.DEBUG if verbose else logging.INFO
+    )
+    logging.captureWarnings(True)
+
+    bootstrap_brokers = bootstrap_brokers.split(',')
+
+    producer = KafkaProducer(
+        bootstrap_servers=bootstrap_brokers,
+        value_serializer=value_serializer,
+        key_serializer=key_serializer,
+        # TODO: make configurable
+        acks='all'
+    )
+
+    try:
+        for line in sys.stdin:
+            message = json.loads(line)
+            value = message['value']
+            value_string = value
+
+            if json_value:
+                value_string = json.dumps(value,
+                                          indent=True,
+                                          ensure_ascii=False,
+                                          sort_keys=True)
+
+            args = {
+                'topic': topic,
+                'value': value_string
+            }
+
+            if 'key' in message:
+                args['key'] = message['key']
+
+            if 'partition' in message:
+                args['partition'] = message['partition']
+
+            producer.send(**args)
+
+    except KeyboardInterrupt:
+        pass
+
+    producer.flush()
+    producer.close()
+
+
 main.add_command(consume)
+main.add_command(produce)
